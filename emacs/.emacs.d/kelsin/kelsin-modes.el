c6078724b08c0dd9a803932410bbc6218ba89453
Christopher Giroir <cgiroir@blizzard.com>
2016-07-05 09:22:21 -0700

    Moving to web-mode

--- emacs/.emacs.d/kelsin/kelsin-modes.el
+++ emacs/.emacs.d/kelsin/kelsin-modes.el
@@ -89,15 +89,15 @@
 (autoload 'magit-get-current-branch "magit" t)
 
 ;; Web Mode
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.jsp\\'" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.ftl\\'" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . nxml-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ftl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 ;; Java Settings
 (require 'kelsin-java)
