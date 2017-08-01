cf844139fd2e6a3dec98c1a4418f53e4fbcd624f
Christopher Giroir <cgiroir@blizzard.com>
2017-08-01 09:44:42 -0700

    Adding ledger stuff

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -102,6 +102,11 @@
   (global-prettify-symbols-mode 1)
   (setq prettify-symbols-unprettify-at-point 'right-edge)
 
+  ;; Ledger Mode
+  (use-package ledger-mode
+    :ensure t
+    :mode "\\.ledger\\'")
+
   ;; Linum Mode
   (use-package linum
     :ensure t
