a15efb0d66240704e7643a5bfa805f28a5f2dad1
Christopher Giroir <kelsin@valefor.com>
2021-11-30 10:52:10 -0800

    feat: new src folder

--- zsh/.zsh/directories.zsh
+++ zsh/.zsh/directories.zsh
@@ -5,7 +5,7 @@ typeset -A dir_names
 dir_names+=($HOME home)
 dir_names+=($HOME/src src)
 dir_names+=($HOME/src/18xx 18xx.games)
-dir_names+=($HOME/src/improbable improbable)
+dir_names+=($HOME/src/airbnb airbnb)
 dir_names+=($HOME/src/18xx-maker 18xx-maker)
 dir_names+=($HOME/src/18xx-maker/18xx-maker 18xx-maker/site)
 dir_names+=($HOME/src/18xx-maker/games 18xx-maker/games)
