0846bab8719fdeea398eff884599dfaf021a20bb
Christopher Giroir <kelsin@valefor.com>
2025-11-21 08:36:21 -0800

    chore: more catppuccin themes

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -3,13 +3,6 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-        require("catppuccin").setup({
-            transparent_background = true,
-            float = {
-                transparent = false,
-                solid = false,
-            },
-        })
         vim.cmd.colorscheme("catppuccin")
     end,
 }
